f394d4c074945ada696f32b99bba66b0e01abb7e
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2018-11-16 17:37:21 -0400

    Inicializando el proyecto de tareas

--- src/App.js
+++ src/App.js
@@ -6,20 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <nav class="navbar navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            <img src={logo} className="App-logo" alt="logo" width="40px" />
+            React.js
           </a>
-        </header>
+        </nav>
+        
       </div>
     );
   }
